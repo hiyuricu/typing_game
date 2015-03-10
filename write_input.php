ff109f1b754fdbc2f6698cdc5d666456370f4c63
ryu <tachibanaryuichi@ryu-no-MacBook-Air.local>
2015-03-10 20:41:36 +0900

    modified

--- write_input.php
+++ write_input.php
@@ -1,5 +1,5 @@
 <?php
 $file = 'sample.txt';
-$current = $_GET['name'];
-file_put_contents($file, $current);
+$current .= $_GET['name'];
+file_put_contents($file, $current, FILE_APPEND | LOCK_EX);
 ?>
